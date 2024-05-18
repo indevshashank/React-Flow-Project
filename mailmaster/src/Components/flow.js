8d4b0e5872156cdc340486bf6a5b2f4b6b6bf5aa
Typicalcodes <indev.shashank@gmail.com>
2024-05-18 22:46:08 +0530

    Some Fixed Done

--- mailmaster/src/Components/flow.js
+++ mailmaster/src/Components/flow.js
@@ -3,24 +3,19 @@ import ReactFlow, {
   addEdge,
   useNodesState,
   useEdgesState,
-  MarkerType,
-  Background,
   Controls,
   ReactFlowProvider,
   useReactFlow,
   getConnectedEdges,
-  Panel,
+
 } from "reactflow";
-import clipboard from "clipboard-copy";
 import "reactflow/dist/style.css";
-import Newtextnode from "./Newtextnode";
-import customNode from "./customnode";
+
 
 import "./btnstyle.css";
 
 import "./styles.css";
 
-import Dropper from "./Dropper";
 import newnoder from "./newnode";
 
 import Plusnode from "./Addnodestyle";
@@ -53,7 +48,6 @@ const getId = () => `dndnode_${nodeid++}`;
 //NOTE The Main Component
 const Flow = () => {
   
-  const reactflow = useReactFlow();
   // NOTE All States
   const [prevplus , setPrevplus] = useState({edgeid: "e2-e1000", source: "2", target: "1000" ,height: 220})
   const [cart, setCart] = useState({})
@@ -63,6 +57,7 @@ const Flow = () => {
   const [edges, setEdge, onEdgesChange] = useEdgesState(initialEdges);
   const [procmodal, setProcmodal] = useState(false)
   const reactflowbox = useRef();
+  const [savestatus, setSavestatus] = useState(false)
   const [reactflowinstance, setReactflowinstance] = useState(null);
   const [doc, setDoc] = useState(null);
   const [mtrigger, setMtrigger]= useState(false)
@@ -71,7 +66,6 @@ const Flow = () => {
   }
  useEffect(() => {
   if(cart.listnames && mtrigger){
-    console.log("i am called")
     setProcmodal(true)
     setMtrigger(false)
   }
@@ -104,124 +98,14 @@ const Flow = () => {
   ];
 
   const [nodes, setNode, onNodesChange] = useNodesState(initialnode);
-  // drag function
-  const dragoverstart = useCallback((event) => {
-    event.preventDefault();
-    event.dataTransfer.dropEffect = "move";
-  }, []);
 
-  //Selecting function
-  const selectionchange = useCallback((event) => {
-    setSelectedNodes(event.nodes);
-    setSelectedEdges(getConnectedEdges(event.nodes, reactflow.getEdges()));
-  }, []);
   useEffect(() => {}, [selectedNodes,doc]);
   useEffect(() => {}, [selectedEdges]);
 
-  //copying
-  function copyItems(event) {
-    const data = {
-      nodes: selectedNodes,
-      edges: selectedEdges,
-    };
-    const ob = JSON.stringify(data);
-    clipboard(ob);
-  }
-  const pasteItems = async () => {
-    const textFromClipboard = await navigator.clipboard.readText();
-    const ob = JSON.parse(textFromClipboard);
-    console.log(ob);
-    const now = Date.now();
-    if (ob.nodes && ob.nodes.length > 0) {
-      ob.nodes.forEach((element) => {
-        element.id = `${element.id}_${now}`;
-      });
-    }
-    if (ob.edges && ob.edges.length > 0) {
-      ob.edges.forEach((element) => {
-        element.source = `${element.source}_${now}`;
-        element.target = `${element.target}_${now}`;
-        element.id = `${element.id}_${now}`;
-      });
-    }
-
-    reactflow.addNodes(ob.nodes);
-    nodes.forEach((element) => {
-      element.selected = false;
-    });
-    setNode((prev) => nodes);
-    setNode((prev) => [...prev, ...ob.nodes]);
-    setEdge((prev) => [...prev, ...ob.edges]);
-  };
-  //NOTE cut function
-  const cutItems = async () => {
-    const data = {
-      nodes: selectedNodes,
-      edges: selectedEdges,
-    };
-    const ob = JSON.stringify(data);
-    clipboard(ob);
-    const newnodes = nodes.filter((item) => {
-      return !selectedNodes.some((removeItem) => removeItem.id === item.id);
-    });
-    console.log(newnodes);
-    setNode(newnodes);
-  };
   const closepros=()=>{
     setProcmodal(false)
   }
 
-  //NOTE drop function
-  const onDrop = useCallback(
-    (event) => {
-      event.preventDefault();
-      const boundingbox = reactflowbox.current.getBoundingClientRect();
-      const type = event.dataTransfer.getData("application/reactflow");
-      const color = event.dataTransfer.getData("application/reactflowcolor");
-      const label = event.dataTransfer.getData("application/reactflowlabel");
-
-      if (typeof type === "undefined" || !type) {
-        return;
-      }
-
-      const position = reactflowinstance.project({
-        x: event.clientX - boundingbox.left,
-        y: event.clientY - boundingbox.top,
-      });
-      const nextnode = {
-        id: getId(),
-        type,
-        position,
-        data: { label: `${label}`, color: `${color}` },
-      };
-      setNode((prev) => prev.concat(nextnode));
-    },
-    [reactflowinstance]
-  );
-
-  // adding edge
-  const addnode = useCallback(() => {
-    const id = `${++nodeid}`;
-    const newtnode = {
-      id,
-
-      data: { label: "New Lable" },
-      position: { x: Math.random() * 200, y: Math.random() * 150 },
-    };
-    setNode((prev) => [...prev, newtnode]);;
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener("copy", copyItems);
-    window.addEventListener("paste", pasteItems);
-    window.addEventListener("cut", cutItems);
-
-    return () => {
-      window.removeEventListener("copy", copyItems);
-      window.removeEventListener("paste", pasteItems);
-      window.removeEventListener("cut", cutItems);
-    };
-  }, [selectedNodes, selectedEdges]);
   const onConnect = useCallback(
     (params) => {
       if (params.source !== params.target) {
@@ -232,56 +116,6 @@ const Flow = () => {
   );
 
 
-  //NOTE save function
-  const saveFlow = useCallback(() => {
-    if (reactflowinstance) {
-      const flow = reactflowinstance.toObject();
-      const string = JSON.stringify(flow);
-
-      const element = document.createElement("a");
-      const file = new Blob([string], {
-        type: "text/plain;charset=utf-8",
-      });
-      element.href = URL.createObjectURL(file);
-      element.download = "myFile.txt";
-      document.body.appendChild(element);
-      element.click();
-    }
-  }, [reactflowinstance]);
-
-//NOTE handlefilesumbit
-function handleFileChange(event) {
-  const file = event.target.files[0];
-
-  if (file) {
-    // Read the contents of the selected file
-    const reader = new FileReader();
-
-    reader.onload = function (e) {
-      // e.target.result contains the text content of the file
-      const fileContent = e.target.result;
-
-      // Now you can do something with the file content, like displaying it in the component's state or rendering it in your UI.
-      setDoc((prev)=>fileContent)
-    };
-
-    // Read the file as text
-    const text = reader.readAsText(file)
-    console.log(text)    
-  }
-}
-// NOTE  loading files  
-  const loadfile = useCallback(() => {
-     const flow = JSON.parse(doc);
-    console.log(doc)
-    if (flow) {
-      
-      setNode(flow.nodes || []);
-      setEdge(flow.edges || []);
-     
-    }
-    
-  }, [doc]);
 
 
  // Event handler to prevent node movement on drag start
@@ -351,12 +185,12 @@ function remove (id){
     
   }
   async function sendtoback (){
+    setSavestatus(false)
     const form = new FormData()
     const formdata = {
       data: cart
       
     }
-    console.log(nodes)
     form.append("data",nodes)
     const response = await fetch("http://localhost:3001/api/createschedule" , {
       method : "POST",
@@ -366,11 +200,17 @@ function remove (id){
       body: JSON.stringify(formdata)
     })
     const json = await response.json();
+    if(json.success){
+      setSavestatus(true)
+    }
     console.log(json)
   }
   return (
     <div className="">
-      <button className="" onClick={()=>{sendtoback()}}>Save and Schedule</button>
+      <div className="flex space-x-2 items-center justify-start p-2 ">
+      <button className="p-2 border rounded-md hover:bg-green-500 hover:text-white " onClick={()=>{if(cart.process && cart.process.length>0){sendtoback()}}}>Save and Schedule</button>
+      {savestatus && <span className="text-green-500 font-bold">Saved and Scheduled Successfully</span>}
+      </div>
       {listmodal && <Listmodal closelist={closelist} changelist={changelist} />}
       {procmodal && <Processmodal closepros={closepros} cart={cart} tempdone={tempdone} />}
       <div
@@ -398,26 +238,7 @@ function remove (id){
 
         >
 
-          <Panel className="flex space-x-2" position="top-left">
-            <button
-              onClick={() => {
-                saveFlow();
-              }}
-              className="bg-black px-4 py-2 text-white font-semibold hover:text-gray-200 rounded"
-            >
-              Save
-            </button>
-          
-            <button
-              onClick={() => {
-                addnode()
-              }}
-              className="bg-black px-4 py-2 text-white font-semibold hover:text-gray-200 rounded"
-            >
-              Add Node
-            </button>
-          
-          </Panel>
+         
          
           <Controls />
         </ReactFlow>
